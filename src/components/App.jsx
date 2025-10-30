a9194f387fa643071b781f4e5afe514d3b2cc1f4
cout-baivab <mty.baivab@gmail.com>
2025-10-30 11:57:34 +0530

    Implement the add note functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const[notes, setNote] = useState([]);
+  
+  function addNote(note){
+    setNote(prevNote => {
+      return [...prevNote, note]
+    })
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea 
+        onAdd = {addNote}
+      />
+      {notes.map(noteItem => {
+        return <Note 
+        title={noteItem.title}
+        content={noteItem.content}
+        />
+      })}
       <Footer />
     </div>
   );
